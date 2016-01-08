ed9b1c0f4bfd31a8b9d9bbccbc79d7bac7486ed5
frtennis1 <frtennis1@gmail.com>
2016-01-08 14:40:30 -0500

    Working on Fingerprint Search Activity

--- src/org/ses/android/soap/tasks/buscarHuellaTask.java
+++ src/org/ses/android/soap/tasks/buscarHuellaTask.java
@@ -1,31 +1,67 @@
 package org.ses.android.soap.tasks;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import org.ses.android.soap.database.Participant;
 
-import SecuGen.FDxSDKPro.JSGFPLib;
-import SecuGen.FDxSDKPro.SGANSITemplateInfo;
-import SecuGen.FDxSDKPro.SGAutoOnEventNotifier;
-import SecuGen.FDxSDKPro.SGFDxDeviceName;
-import SecuGen.FDxSDKPro.SGFDxErrorCode;
-import SecuGen.FDxSDKPro.SGFDxSecurityLevel;
-import SecuGen.FDxSDKPro.SGFDxTemplateFormat;
-import SecuGen.FDxSDKPro.SGFingerInfo;
-import SecuGen.FDxSDKPro.SGFingerPresentEvent;
-import SecuGen.FDxSDKPro.SGISOTemplateInfo;
-import SecuGen.FDxSDKPro.SGImpressionType;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+
 
 /**
  * Created by franciscorivera on 1/8/16.
  */
 public class buscarHuellaTask extends AsyncTask<Byte, Void, Participant> {
 
-    private JSGFPLib sgfplib;
-
     @Override
     protected Participant doInBackground(Byte ... params)
     {
-        return new Participant("", "", "", "", 0, "", "", 0);
+        // return bogus information
+        Participant resul = new Participant("", "", "", "", 0, "", "", 0);
+
+
+        /* what you should actually do--presently unbuilt because of lack of backend */
+
+        final String NAMESPACE = StringConexion.conexion;
+        final String URL=NAMESPACE+"WSSEIS/WSParticipante.asmx";
+        final String METHOD_NAME = "BuscarHuella";
+        final String SOAP_ACTION = NAMESPACE+METHOD_NAME;
+
+        SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+        request.addProperty("huella", params);
+
+        SoapSerializationEnvelope envelope =
+                new SoapSerializationEnvelope(SoapEnvelope.VER11);
+
+        envelope.dotNet = true;
+
+        envelope.setOutputSoapObject(request);
+
+        HttpTransportSE transporte = new HttpTransportSE(URL);
+
+        try
+        {
+            transporte.call(SOAP_ACTION, envelope);
+
+            SoapObject resSoap =(SoapObject)envelope.getResponse();
+            SoapObject ic = (SoapObject)resSoap.getProperty(0);
+
+            Log.i("CodigoPaciente", "res: " + res);
+
+        }
+        catch (Exception e)
+        {
+            resul = "";
+            Log.i("Exception" ,e.getMessage());
+        }
+
+
+        return resul;
+
     }
 }
