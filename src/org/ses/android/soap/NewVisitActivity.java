80a05471cc5fd9e357101b6b5b308ee715752cbd
fanneyzhu <fanneyzhu@gmail.com>
2016-01-13 16:28:48 -0500

    date

--- src/org/ses/android/soap/NewVisitActivity.java
+++ src/org/ses/android/soap/NewVisitActivity.java
@@ -2,6 +2,7 @@ package org.ses.android.soap;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
@@ -16,7 +17,9 @@ import org.ses.android.soap.tasks.StringConexion;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -90,6 +93,10 @@ public class NewVisitActivity extends Activity {
     Date visitDate = new Date();
     Date visitTime = new Date();
 
+    private int year;
+    private int month;
+    private int day;
+
     private TextView names;
     private TextView local;
     private TextView project;
@@ -100,6 +107,8 @@ public class NewVisitActivity extends Activity {
 
     private TextView visit_date;
     private Button btn_save_visit;
+
+    static final int DATE_DIALOG_ID = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,6 +125,9 @@ public class NewVisitActivity extends Activity {
         visit_date = (TextView) findViewById(R.id.visit_date);
 
         btn_save_visit = (Button) findViewById(R.id.btn_save_visit);
+
+        setCurrentDateOnView();
+        addListenerOnVisitDate();
 
         // get localeId, localeName and promoterId from sharedPreferences
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
@@ -218,6 +230,76 @@ public class NewVisitActivity extends Activity {
             visitDate = date;
         } */
     }
+
+    public void setCurrentDateOnView() {
+
+        final Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+        // set current date into textview
+        visit_date.setText(new StringBuilder()
+                // Month is 0 based, just add 1
+                .append(day).append("/").append(month + 1).append("/")
+                .append(year));
+
+    }
+
+    public void addListenerOnVisitDate() {
+
+        visit_date.setOnTouchListener(new View.OnTouchListener() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                showDialog(DATE_DIALOG_ID);
+                return true;
+            }
+
+
+        });
+        visit_date.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onClick(View v) {
+
+                showDialog(DATE_DIALOG_ID);
+
+            }
+
+        });
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                // set date picker as current date
+                return new DatePickerDialog(this, datePickerListener,
+                        year, month,day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener
+            = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        @Override
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            year = selectedYear;
+            month = selectedMonth;
+            day = selectedDay;
+
+            // set selected date into textview
+            visit_date.setText(day + "/" + (month + 1) + ("/") + (year));
+
+        }
+    };
 
 }
 
