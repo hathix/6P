8bc6e97d1eb7464bf14a66a0bf0db4ef2c7ded04
Neel Mehta <neelmehta@college.harvard.edu>
2016-01-19 11:12:54 -0500

    Hotfix: add default constructor

--- src/org/ses/android/soap/models/Cacheable.java
+++ src/org/ses/android/soap/models/Cacheable.java
@@ -9,6 +9,11 @@ public class Cacheable {
 
     public JSONObject jsonObject = null;
 
+    /**
+     * Needed as a default constructor because it's subclassed.
+     */
+    public Cacheable() {}
+
     public Cacheable(JSONObject jsonObject) { this.jsonObject = jsonObject; }
 
     public JSONObject toJSON() { return new JSONObject(); }
